8a8a89fe3cc2eb2ad84960d1f09655cb7014dc88
LaOngDaoxing <18731197300@163.com>
2020-11-05 13:29:22 +0800

    二、动态自定义组件-父向子传递数据

---
+++ components/NavTabsA/NavTabsA.js
@@ -0,0 +1,98 @@
+// components/Tabs/TabsA.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+      tabsArr1:[
+        {
+          id:0,
+          name:"首页",
+          isActive:true
+        },
+        {
+          id:1,
+          name:"原创",
+          isActive:false
+        },
+        {
+          id:2,
+          name:"分类",
+          isActive:false
+        },
+        {
+          id:3,
+          name:"关于",
+          isActive:false
+        }
+      ],
+      tabsArr2:[
+        {
+          id:0,
+          name:"首页",
+          isActive:true
+        },
+        {
+          id:1,
+          name:"原创",
+          isActive:false
+        },
+        {
+          id:2,
+          name:"分类",
+          isActive:false
+        },
+        {
+          id:3,
+          name:"关于",
+          isActive:false
+        }
+      ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    /* 
+    * 1、组件.js 文件中 存放事件回调函数的时候 必须要存在在 methods中！！！
+    * 2、页面.js 文件中 存放事件回调函数的时候 存放在data同层级下！！！
+    */
+    changeTabEventFun(e){
+      /* 
+      1 绑定点击事件  需要在methods中绑定
+      2 获取被点击的索引 
+      3 获取data中的原数组tabsArr 
+        解构  对 复杂类型进行结构的时候 复制了一份 变量的引用而已；不要直接修改 this.data.数据，[].forEach 遍历数组 遍历数组的时候 修改了 v ，也会导致源数组被修改。
+          let {tabsArr}=this.data; 等价于 let tabsArr2Var=this.data.tabsArr2;
+        最严谨的做法 重新拷贝一份 数组，再对这个数组的备份进行处理。
+          let tabsArr2Var=JSON.parse(JSON.stringify(this.data.tabsArr2)); 
+      4 循环原数组
+        1 给每一个循环性 选中属性 改为 false
+        2 给 当前的索引的 项 添加激活选中效果就可以了！！！
+          [].forEach 遍历数组 遍历数组的时候 修改了 v ，也会导致源数组被修改
+      5 点击事件触发的时候 
+          触发父组件中的自定义事件 同时传递数据给  父组件  
+          this.triggerEvent("父组件自定义事件的名称",要传递的参数)
+      */
+      // 2 获取被点击的索引
+      const {index}=e.currentTarget.dataset;
+      // 5 触发父组件中的自定义事件 同时传递数据给  父组件 
+      this.triggerEvent("itemChange",{index});
+      // 3 获取data中的原数组tabsArr 
+      let tabsArr2Var=JSON.parse(JSON.stringify(this.data.tabsArr2));
+      // 4 循环原数组
+      tabsArr2Var.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+      this.setData({
+        tabsArr2:tabsArr2Var
+      })
+    }
+  }
+})
---
+++ components/NavTabsB/NavTabsB.js
@@ -0,0 +1,33 @@
+// components/TabsB/TabsB.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 要接收的数据的名称cusPropertyName001
+    // cusPropertyName001:{
+    //   // type  要接收的数据的类型 
+    //   type:String,
+    //   // value  默认值
+    //   value:""
+    // },
+    tabsArr3:{
+      type:Array,
+      value:[]
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
